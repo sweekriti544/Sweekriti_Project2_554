c50f86fb214a3aeb1d3a9b2e4b424c1e22f44d6d
sweekriti19 <gautamsweekriti@gmail.com>
2025-11-15 22:08:56 -0600

    terraform-k8s

--- terraform-k8s/main.tf
+++ terraform-k8s/main.tf
@@ -1,11 +1,7 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-    helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/kubernetes"
       version = "~> 2.0"
     }
   }
@@ -15,20 +11,122 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
 module "namespace" {
   source = "./modules/namespace"
   name   = "demo"
 }
 
-resource "helm_release" "nginx" {
-  name       = "demo-nginx"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx"
-  namespace  = module.namespace.name
+resource "kubernetes_config_map" "nginx_html" {
+  metadata {
+    name      = "nginx-html"
+    namespace = module.namespace.name
+  }
+
+  data = {
+    "index.html" = file("${path.module}/nginx-html/index.html")
+  }
+}
+
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "demo-nginx"
+    namespace = module.namespace.name
+    labels = {
+      app = "demo-nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "demo-nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "demo-nginx"
+        }
+      }
+
+      spec {
+        # New volume for custom HTML
+        volume {
+          name = "html"
+          config_map {
+            name = kubernetes_config_map.nginx_html.metadata[0].name
+          }
+        }
+
+        container {
+          name  = "nginx"
+          image = "nginx:1.27"
+
+          port {
+            container_port = 80
+          }
+
+          volume_mount {
+            mount_path = "/usr/share/nginx/html"
+            name       = "html"
+          }
+        }
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "demo-nginx"
+    namespace = module.namespace.name
+  }
+
+  spec {
+    type = "NodePort"
+
+    selector = {
+      app = "demo-nginx"
+    }
+
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 80
+      node_port   = 30080
+    }
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler_v2" "nginx_hpa" {
+  metadata {
+    name      = "demo-nginx-hpa"
+    namespace = module.namespace.name
+  }
+
+  spec {
+    min_replicas = 1
+    max_replicas = 3
+
+    scale_target_ref {
+      api_version = "apps/v1"
+      kind        = "Deployment"
+      name        = kubernetes_deployment.nginx.metadata[0].name
+    }
+
+    metric {
+      type = "Resource"
+      resource {
+        name = "cpu"
+        target {
+          type                = "Utilization"
+          average_utilization = 50
+        }
+      }
+    }
+  }
 }
